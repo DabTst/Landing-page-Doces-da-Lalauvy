db9354868195943de547fac3b4207847bb28426d
Djane Bucar <140601608+DabTst@users.noreply.github.com>
2024-08-26 13:33:33 -0930

    Add files via upload

---
+++ Doces da Lalauvy/scripts.js
@@ -0,0 +1,9 @@
+//Button HamBurguer
+
+const menu = document.querySelector(".menu");
+const NavMenu = document.querySelector(".nav-menu");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("ativo");
+  NavMenu.classList.toggle("ativo");
+});
